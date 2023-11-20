e4f67ad84c3690610de7ebad33e0c389f59da4bd
venky210 <vitsl.20735a0210@gmail.com>
2023-11-20 20:26:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def pavan(request):
+    return render(request,'pavan.html')
+
+def pavan2(request):
+    return render(request,'pavan2.html')
